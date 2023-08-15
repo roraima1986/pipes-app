b61d8bd0b9a5475b13713b77822541767c7168d3
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-08-15 17:34:28 -0400

    Modulos y Rutas de mi aplicacion

---
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basics-page',
+  templateUrl: './basics-page.component.html',
+  styles: [
+  ]
+})
+export class BasicsPageComponent {
+
+}
