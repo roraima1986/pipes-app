aa696815f4b1a65e05080b253cc068522654fdda
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-08-15 22:32:20 -0400

    i18nSelect Pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
   ]
 })
 export class UncommonPageComponent {
+  //i18n Select
+  public name:string = 'Fernando';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    'male':'invitarlo',
+    'female':'invitarla'
+  };
 
+  changeClient(){
+    this.name = 'Melissa';
+    this.gender = 'female';
+  }
 }
