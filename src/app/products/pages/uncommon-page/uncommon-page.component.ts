5c20ed4ddcd4f3ed8317f2f309c013c7aa266f1e
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-08-15 23:28:47 -0400

    Async Pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -33,11 +34,26 @@ export class UncommonPageComponent {
     this.clients.shift();
   }
 
-  //keyValue
+  //keyValue Pipe
   public person = {
     name: 'Fernando',
     age:36,
     address: 'Otawa, Canada'
   }
+
+  //Async Pipe
+  public myObservableTimer:Observable<number> = interval(2000).pipe(
+    tap(value => console.log('tap', value))
+  );
+
+  public promiseValue:Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data en la promesa');
+      console.log('Tenemos data en la promesa');
+      this.person.name = 'Otro nombre';
+    }, 3500);
+  });
+
+
 
 }
